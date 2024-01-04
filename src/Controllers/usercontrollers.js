9d42586efd7dd5d13a9b007053ee8029a7d86622
Biplove <93914620+Biplove82@users.noreply.github.com>
2024-01-04 19:35:34 +0530

    gitig

--- src/Controllers/usercontrollers.js
+++ src/Controllers/usercontrollers.js
@@ -1,55 +1,48 @@
 const userModells = require("../Modells/usermodells");
-const bcrypt=require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "your-secret-key";
 
-const register = async function(req,res){
-    try {
-      const{
-        name,
-        email,
-        password,
-        confirmPassword,
-      }=req.body;
-      const existingUser = await userModells.findOne({ email:email });
+const register = async function (req, res) {
+  try {
+    const { name, email, password, confirmPassword } = req.body;
+    const existingUser = await userModells.findOne({ email: email });
     if (existingUser) {
       return res.status(401).json({ message: "Username already exists" });
     }
     //Checking the Password and Confirm Password are same or not
-    if (password !== confirmPassword ) {
-        return res.status(422).json({ message: "Passwords do not match"});
+    if (password !== confirmPassword) {
+      return res.status(422).json({ message: "Passwords do not match" });
     }
-    let hashedPassword = await  bcrypt.hash(password, 10)
-    const newUser = new userModells ({
-        name :name ,
-        email :email ,
-        password :hashedPassword
-        })
-        const result = await newUser.save();
-        res
+    let hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new userModells({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+     await newUser.save();
+    res
       .status(200)
       .json({ id: newUser._id, msg: "User Registered Succesfully" });
-    } catch (error) {
-        console.log(`Error in register ${error}`);
-    };
+  } catch (error) {
+    console.log(`Error in register ${error}`);
+  }
+};
+
+const login = async function (req, res) {
+  let { email, password } = req.body;
+  try {
+    let user = await userModells.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Invalid username or password" });
     }
 
-   
-      const login = async function (req, res) {
-        let { email, password } = req.body;
-        try {
-          let user = await userModells.findOne({ email });
-          if (!user || !(await bcrypt.compare(password, user.password))) {
-            return res.status(401).json({ error: "Invalid username or password" });
-          }
-      
-          let token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
-      
-          res.json({ token, userId: user._id });
-        } catch (error) {
-          res.status(500).json({ error: "Invalid User" +error});
-        }
-      };
-    
-      module.exports={register,login};
-    
+    let token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
+
+    res.json({ token, userId: user._id });
+  } catch (error) {
+    res.status(500).json({ error: "Invalid User" + error });
+  }
+};
+
+module.exports = { register, login };
--- src/Modells/usermodells.js
+++ src/Modells/usermodells.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true,trim:true },
-    password: { type: String},
-    confirmPassword:{ type: String},
-    email: {type:String ,required : true} 
-
-},{timestamps:true})
-module.exports=mongoose.model("user",userSchema);
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    password: { type: String },
+    confirmPassword: { type: String },
+    email: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("user", userSchema);
